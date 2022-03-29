f779f95180b7ca6c7447fafc07f797000e5cff88
Farès <faresboukhezna@gmail.com>
2022-03-29 03:25:12 +0200

    feat: country count wallet

--- src/api/users.js
+++ src/api/users.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const uri = "http://localhost:5500/users/";
 const paginationUrl = `${uri}/pagination/`;
 const countUrl = `${uri}count`;
+const genderUrl = `${uri}stats/gender`;
+const countryUrl = `${uri}stats/country`;
 
 
 export const GetAllUsers = async (page) => {
@@ -14,6 +16,10 @@ export const GetUsersCount = async () => {
 }
 
 export const GetGenderCount = async ()=> {
-  return axios.get()
+  return axios.get(genderUrl);
+}
+
+export const GetCountryCount = async () => {
+  return axios.get(countryUrl);
 }
 
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
-import { GetUsersCount } from '../api/users';
+import { GetCountryCount, GetGenderCount, GetUsersCount } from '../api/users';
+import { RadialChart } from 'react-vis';
 
 const HomePage = () => {
 
@@ -9,8 +11,12 @@ const HomePage = () => {
       <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12 flex justify-between items-center">
         <UserWallet />
       </div>
-      <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12">heyo</div>
-      <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12">heyo</div>
+      <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12">
+        <GenderWallet />
+      </div>
+      <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12">
+        <CountryWallet />
+      </div>
 
       <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12 row-span-3">heyo</div>
       <div className="shadow-2xl bg-white shadow-blue-300/30 rounded-xl p-12 col-span-3 row-span-2">heyo</div>
@@ -38,17 +44,64 @@ const UserWallet = () => {
 
 const GenderWallet = () => {
 
-  const { data, isLoading, error } = useQuery('usercount', () => GetUsersCount());
+
+  const [genderChartData, setGenderChartData] = useState(null);
+  const { data, isLoading, error } = useQuery('gendercount', () => GetGenderCount(), {
+    onSuccess: (data) => {
+      let genderCount = data.data;
+      let genderChartArray = Object.keys(genderCount).map((key) => ({angle: genderCount[key], label: key}));
+      setGenderChartData(genderChartArray);
+    }
+  });
 
   if (isLoading) return 'loading...'
 
   return(
     <div className='h-full w-full flex flex-col gap-4 justify-center items-center'>
-      <h2 className='font-bold text-xl uppercase'>Utilisateurs</h2>
-      <strong className='xl:text-8xl lg:text-6xl text-3xl'>{data.data}</strong>
+      <h2 className='font-bold text-xl uppercase'>Répartition par genre</h2>
+      <RadialChart data={genderChartData} height={200} width={200} innerRaidus={68} radius={75} showLabels/>
     </div>
   )
 
+}
+
+const CountryWallet = () => {
+
+
+  const [countryChartData, setCountryChartData] = useState(null);
+  const { data, isLoading, error } = useQuery('countrycount', () => GetCountryCount(), {
+    onSuccess: (data) => {
+      let countryCount = data.data;
+      let countryChartArray = Object.keys(countryCount).map((key) => ({angle: countryCount[key], label: key}));
+      setCountryChartData(countryChartArray);
+    }
+  });
+
+  if (isLoading) return 'loading...'
+
+  return(
+    <div className='h-full w-full flex flex-col gap-4 justify-center items-center'>
+      <h2 className='font-bold text-xl uppercase'>Répartition par pays</h2>
+      <RadialChart data={countryChartData} height={200} width={200} innerRaidus={68} radius={75} showLabels/>
+    </div>
+  )
+
+}
+
+const CountryTopList = () =>{
+  const { data, isLoading, error } = useQuery('countrycount', () => GetCountryCount(), {
+    onSuccess: (data) => {
+      
+    }
+  });
+
+  if (isLoading) return 'loading...'
+
+  return(
+    <div className='h-full w-full flex flex-col gap-4 justify-center items-center'>
+      <h2 className='font-bold text-xl uppercase'>Top 15 des pays</h2>
+    </div>
+  )
 }
 
 export default HomePage;
